0c6bd69445017664a8c266872d614cc4c026484d
wangqinghong <923793518@qq.com>
2018-11-18 14:40:13 -0500

    Employer

--- src/main/java/com/youthchina/YouthchinaApplication.java
+++ src/main/java/com/youthchina/YouthchinaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:/config.properties")
-@MapperScan("com.youthchina.*.mapper")
+
 public class YouthchinaApplication {
 
     public static void main(String[] args) {
