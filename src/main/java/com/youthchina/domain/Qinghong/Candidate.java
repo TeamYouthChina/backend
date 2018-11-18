a433db1a99750cb402f58d33e29bfa25dd39decc
wangqinghong <923793518@qq.com>
2018-11-18 14:33:45 -0500

    Employer

---
+++ src/main/java/com/youthchina/domain/Qinghong/Candidate.java
@@ -0,0 +1,151 @@
+package com.youthchina.domain.Qinghong;
+
+public class Candidate {
+
+    private Integer id;
+    private String talent_label_one;
+    private String talent_label_two;
+    private String talent_label_three;
+    private String job_intention_one;
+    private String job_intention_two;
+    private String job_intention_three;
+    private String job_intention_four;
+    private String duty_time;
+    private String desire_industry_one;
+    private String desire_industry_two;
+    private String desire_industry_three;
+    private String desire_industry_four;
+    private String desire_location_one;
+    private String desire_location_two;
+    private String desire_location_three;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTalent_label_one() {
+        return talent_label_one;
+    }
+
+    public void setTalent_label_one(String talent_label_one) {
+        this.talent_label_one = talent_label_one;
+    }
+
+    public String getTalent_label_two() {
+        return talent_label_two;
+    }
+
+    public void setTalent_label_two(String talent_label_two) {
+        this.talent_label_two = talent_label_two;
+    }
+
+    public String getTalent_label_three() {
+        return talent_label_three;
+    }
+
+    public void setTalent_label_three(String talent_label_three) {
+        this.talent_label_three = talent_label_three;
+    }
+
+    public String getJob_intention_one() {
+        return job_intention_one;
+    }
+
+    public void setJob_intention_one(String job_intention_one) {
+        this.job_intention_one = job_intention_one;
+    }
+
+    public String getJob_intention_two() {
+        return job_intention_two;
+    }
+
+    public void setJob_intention_two(String job_intention_two) {
+        this.job_intention_two = job_intention_two;
+    }
+
+    public String getJob_intention_three() {
+        return job_intention_three;
+    }
+
+    public void setJob_intention_three(String job_intention_three) {
+        this.job_intention_three = job_intention_three;
+    }
+
+    public String getJob_intention_four() {
+        return job_intention_four;
+    }
+
+    public void setJob_intention_four(String job_intention_four) {
+        this.job_intention_four = job_intention_four;
+    }
+
+    public String getDuty_time() {
+        return duty_time;
+    }
+
+    public void setDuty_time(String duty_time) {
+        this.duty_time = duty_time;
+    }
+
+    public String getDesire_industry_one() {
+        return desire_industry_one;
+    }
+
+    public void setDesire_industry_one(String desire_industry_one) {
+        this.desire_industry_one = desire_industry_one;
+    }
+
+    public String getDesire_industry_two() {
+        return desire_industry_two;
+    }
+
+    public void setDesire_industry_two(String desire_industry_two) {
+        this.desire_industry_two = desire_industry_two;
+    }
+
+    public String getDesire_industry_three() {
+        return desire_industry_three;
+    }
+
+    public void setDesire_industry_three(String desire_industry_three) {
+        this.desire_industry_three = desire_industry_three;
+    }
+
+    public String getDesire_industry_four() {
+        return desire_industry_four;
+    }
+
+    public void setDesire_industry_four(String desire_industry_four) {
+        this.desire_industry_four = desire_industry_four;
+    }
+
+    public String getDesire_location_one() {
+        return desire_location_one;
+    }
+
+    public void setDesire_location_one(String desire_location_one) {
+        this.desire_location_one = desire_location_one;
+    }
+
+    public String getDesire_location_two() {
+        return desire_location_two;
+    }
+
+    public void setDesire_location_two(String desire_location_two) {
+        this.desire_location_two = desire_location_two;
+    }
+
+    public String getDesire_location_three() {
+        return desire_location_three;
+    }
+
+    public void setDesire_location_three(String desire_location_three) {
+        this.desire_location_three = desire_location_three;
+    }
+}
+
+
---
+++ src/main/java/com/youthchina/domain/Qinghong/InviationMapper.java
@@ -0,0 +1,22 @@
+package com.youthchina.domain.Qinghong;
+
+public class InviationMapper {
+    private int invite_id;
+    private int user_id;
+
+    public int getInvite_id() {
+        return invite_id;
+    }
+
+    public void setInvite_id(int invite_id) {
+        this.invite_id = invite_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
