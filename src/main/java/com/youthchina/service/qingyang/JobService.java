923dd56a9053aa6d7d3a12e872e2439616e8910d
IrvingZ <irvingzqy@gmail.com>
2018-11-24 23:59:53 -0500

    职位Job修改 Resume 异常类型修改

--- src/main/java/com/youthchina/service/qingyang/JobService.java
+++ src/main/java/com/youthchina/service/qingyang/JobService.java
@@ -1,25 +1,19 @@
 package com.youthchina.service.qingyang;
 
 import com.youthchina.domain.qingyang.Job_qingyang;
+import com.youthchina.domain.zhongyang.User;
+import com.youthchina.exception.zhongyang.NotBelongException;
+import com.youthchina.exception.zhongyang.NotFoundException;
+import com.youthchina.service.DomainCRUDService;
 
-import java.util.List;
-import java.util.Map;
+public interface JobService extends DomainCRUDService<Job_qingyang, String>{
 
-public interface JobService {
-    /**添加Job*/
-    Map<String,Object> insertJob(Job_qingyang job);
 
-    /**修改Job信息*/
-    Map<String,Object> updateJob(Job_qingyang job);
+    /**HR删除Job信息*/
+    void delete(User user, Integer jobId) throws NotFoundException;
 
-    /**删除Job信息*/
-    Map<String,Object> deleteJob(String job_id);
-
-    /**通过JobID查询Job信息*/
-    Job_qingyang getJobByJobId(String job_id);
-
-    /**通过CompanyID查询Job信息*/
-    List<Job_qingyang> getJobByComId(String company_id);
+    /**HR通过JobID查询Job信息, 能不能edit*/
+    Job_qingyang getByHr(User user, Integer jobId) throws NotBelongException;
 
 
 }
