d0d144b25a44d973a31cc5efc755558be669463e
zhongyang.wu <zhongyang.wu@outlook.com>
2019-01-02 10:43:21 -0500

    Add test for login

--- src/test/java/com/youthchina/controller/UserControllerTest.java
+++ src/test/java/com/youthchina/controller/UserControllerTest.java
@@ -1,10 +1,62 @@
 package com.youthchina.controller;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.apache.catalina.core.ApplicationContext;
+import org.apache.commons.collections4.map.LinkedMap;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Map;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
 /**
  * Created by zhongyangwu on 1/2/19.
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class, TransactionalTestExecutionListener.class})
+@DatabaseSetup({"classpath:users.xml"})
+@WebAppConfiguration
 public class UserControllerTest {
+    @Autowired
+    WebApplicationContext context;
+
+    @Value("${web.url.prefix}")
+    private String urlPrefix;
+
+    MockMvc mvc;
+
+    @Before
+    public void setup() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        this.mvc.perform(post(this.urlPrefix + "/login").param("id", "1").param("password", "123456"))
+                .andDo(print())
+                .andExpect(content().json("{\"content\":{\"id\":1,\"username\":\"yihao guo\",\"email\":null,\"phonenumber\":\"18463722634\",\"registerDate\":\"2018-10-11 11:11:22.0\",\"realName\":\"None\",\"gender\":\"male\",\"nation\":\"China\",\"avatarUrl\":null,\"role\":1,\"age\":21},\"status\":{\"code\":2000,\"reason\":\"\"}}", false))
+                .andExpect(header().exists("X-AUTHENTICATION"))
+        ;
+
+    }
 }
