2b2547bd49a725ca69d7b3c74b45529ff7ea0bfc
JinhaoHu <hujinhao0401@gmail.com>
2019-01-05 19:25:51 -0500

    全部测试完成

--- src/main/java/com/youthchina/controller/zhongyang/JobController.java
+++ src/main/java/com/youthchina/controller/zhongyang/JobController.java
@@ -10,6 +10,7 @@ import com.youthchina.service.qingyang.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+
 import org.springframework.web.bind.annotation.*;
 
 /**
