f8058f229f832153a9677b8e97829944194634b9
IrvingZ <irvingzqy@gmail.com>
2019-02-10 16:43:17 -0500

    backend-103 Student Controller

--- src/test/java/com/youthchina/Qingyang/StudentControllerTest.java
+++ src/test/java/com/youthchina/Qingyang/StudentControllerTest.java
@@ -1,8 +1,9 @@
 package com.youthchina.Qingyang;
 
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.youthchina.dto.ApplicantDTO;
+import com.youthchina.dto.*;
 import com.youthchina.util.AuthGenerator;
 
 import org.junit.Before;
@@ -33,7 +34,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.equalTo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RunWith(SpringRunner.class)
@@ -150,49 +153,60 @@ public class StudentControllerTest {
         applicantDTO.setName("Irving");
         applicantDTO.setAvatarUrl("IrvingUrl");
 
-        Map<String, Object> applicantMap = new HashMap<>();
-        //applicantMap.put("id", Integer.valueOf(2));
-        applicantMap.put("name", "Irving");
-        applicantMap.put("avatarUrl", "IrvingUrl");
-        applicantMap.put("isInJob", true);
+         List<EducationDTO> educations = new ArrayList<>();
+         applicantDTO.setEducations(educations);
+         List<String> emails = new ArrayList<>();
+         applicantDTO.setEmails(emails);
+         List<String> phonenumbers = new ArrayList<>();
+         applicantDTO.setPhonenumbers(phonenumbers);
+         List<WorkDTO> experiences = new ArrayList<>();
+         applicantDTO.setExperiences(experiences);
+         List<ProjectDTO> projects = new ArrayList<>();
+         applicantDTO.setProjects(projects);
+         List<ExtracurricularDTO> extracurriculars = new ArrayList<>();
+         applicantDTO.setExtracurriculars(extracurriculars);
+         List<CertificateDTO> certificates = new ArrayList<>();
+         applicantDTO.setCertificates(certificates);
 
-        Map<String, Object> currentCompany = new HashMap<>();
-        currentCompany.put("id", "1");
-        currentCompany.put("name", "baidu");
-        currentCompany.put("avatarUrl", "baiduUrl");
-        applicantMap.put("currentCompany", currentCompany);
-
-        Map<String, Object> educations = null;//TODO
-        applicantMap.put("educations", educations);
-
-        Map<String, Object> contacts = new HashMap<>();
-        contacts.put("emails", "irving@baidu.com");
-        contacts.put("phonenumbers", "2022022202");
-        applicantMap.put("contacts", contacts);
-
-        Map<String, Object> works = new HashMap<>(); //TODO
-        applicantMap.put("works", works);
-
-        Map<String, Object> projects = new HashMap<>(); //TODO
-        applicantMap.put("projects", projects);
-
-        Map<String, Object> extracurriculars = new HashMap<>(); //TODO
-        applicantMap.put("extracurriculars", extracurriculars);
-
-        Map<String, Object> certifications = new HashMap<>(); //TODO
-        applicantMap.put("certifications", certifications);
-
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        java.lang.String searchJson = ow.writeValueAsString(applicantDTO);
 
         this.mvc.perform(
                 post(this.urlPrefix + "/applicants")
                         .with(authGenerator.authentication())
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
-                    //.content(new JasonObject)
-                    .content(new ObjectMapper().writeValueAsString(applicantMap))
+                    .content(searchJson)
         )
                 .andDo(print())
         ;
     }
+
+//    @Test
+//    public void testSearch() throws Exception {
+//        JobSearchDTO jobSearchDTO = new JobSearchDTO();
+//        jobSearchDTO.setJobId(1);
+//        jobSearchDTO.setJobName("front");
+//        jobSearchDTO.setComId(1);
+//        jobSearchDTO.setComName("大疆");
+//        jobSearchDTO.setType(1);
+//        jobSearchDTO.setSalaryCap(6000);
+//        jobSearchDTO.setSalaryFloor(5000);
+//        jobSearchDTO.setActive(1);
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+//        java.lang.String searchJson = ow.writeValueAsString(jobSearchDTO);
+//
+//        this.mvc.perform(
+//                post(this.urlPrefix + "/jobs/search").contentType(MediaType.APPLICATION_JSON_UTF8)
+//                        .content(searchJson)
+//                        .with(authGenerator.authentication())
+//        )
+//                .andDo(print())
+//                .andExpect(content().json("{\"content\":{\"searchResult\":[{\"jobId\":1,\"jobName\":\"front\",\"jobProfCode\":\"A\",\"jobStartTime\":\"2019-01-01\",\"jobEndTime\":\"2020-01-01\",\"jobType\":1,\"jobDescription\":\"996\",\"jobDuty\":\"front\",\"jobHighlight\":\"50K\",\"jobSalaryFloor\":5000,\"jobSalaryCap\":6000,\"jobLink\":\"job.com\",\"cvReceiMail\":\"youth@china\",\"cvNameRule\":\"nameRule\",\"jobActive\":1,\"jobLocationList\":[{\"region_num\":1,\"region_chn\":\"北京\",\"region_eng\":\"Beijing\",\"region_level\":1,\"region_parent_num\":1,\"start_time\":\"2019-01-01T11:11:22.000+0000\",\"is_delete\":null,\"is_delete_time\":null,\"jobId\":1}],\"jobReqList\":[{\"degreeNum\":1,\"degreeChn\":\"本科\",\"degreeEng\":\"Bachelor\",\"startDate\":\"2019-01-01T11:11:22.000+0000\",\"jobId\":1},{\"degreeNum\":2,\"degreeChn\":\"硕士\",\"degreeEng\":\"Master\",\"startDate\":\"2019-01-02T11:11:22.000+0000\",\"jobId\":1}],\"industries\":[{\"indNum\":1,\"indCode\":\"A\",\"indChn\":\"工\",\"indEng\":\"eng\",\"indLevel\":2,\"indParentCode\":\"A3\",\"startTime\":\"2018-10-11T11:11:22.000+0000\",\"isDelete\":null,\"isDeleteTime\":null,\"companyId\":null,\"jobId\":1},{\"indNum\":2,\"indCode\":\"B\",\"indChn\":\"农\",\"indEng\":\"eng\",\"indLevel\":2,\"indParentCode\":\"B3\",\"startTime\":\"2018-10-11T11:11:22.000+0000\",\"isDelete\":null,\"isDeleteTime\":null,\"companyId\":null,\"jobId\":1}],\"profession\":{\"profNum\":1,\"profCode\":\"A\",\"profParentCode\":\"A\",\"profChn\":\"前端\",\"profEng\":\"frontEnd\",\"startTime\":\"2019-01-01T11:11:22.000+0000\"},\"isDelete\":null,\"isDeleteTime\":null,\"company\":{\"companyId\":1,\"companyName\":\"大疆\",\"companyCode\":\"2\",\"companyIntroduc\":\"无人机\",\"companyNature\":{\"natureNum\":1,\"natureChn\":\"国企\",\"natureEng\":\"public\",\"natureDetail\":\"good\",\"startTime\":\"2019-01-01T11:11:22.000+0000\"},\"companyScale\":{\"scaleNum\":1,\"scaleChn\":\"大\",\"scaleEng\":\"big\",\"startTime\":\"2019-01-01T11:11:22.000+0000\"},\"location\":{\"region_num\":1,\"region_chn\":\"北京\",\"region_eng\":\"Beijing\",\"region_level\":1,\"region_parent_num\":1,\"start_time\":\"2019-01-01T11:11:22.000+0000\",\"is_delete\":null,\"is_delete_time\":null,\"jobId\":null},\"country\":null,\"companyMail\":\"dji@com\",\"companyWebsite\":\"dji.com\",\"companyStartDate\":\"2005-11-20\",\"companyLogo\":\"1\",\"companyVerify\":1,\"userId\":null,\"isDelete\":null,\"isDeleteTime\":null,\"jobs\":null,\"indList\":[{\"indNum\":1,\"indCode\":\"A\",\"indChn\":\"工\",\"indEng\":\"eng\",\"indLevel\":2,\"indParentCode\":\"A3\",\"startTime\":null,\"isDelete\":null,\"isDeleteTime\":null,\"companyId\":1,\"jobId\":null},{\"indNum\":2,\"indCode\":\"B\",\"indChn\":\"农\",\"indEng\":\"eng\",\"indLevel\":2,\"indParentCode\":\"B3\",\"startTime\":null,\"isDelete\":null,\"isDeleteTime\":null,\"companyId\":1,\"jobId\":null}],\"verificationList\":[]},\"hr\":{\"hrId\":1,\"companyId\":1,\"hrOnJob\":1,\"userId\":null,\"isDelete\":null,\"isDeleteTime\":null},\"id\":1}],\"status\":null},\"status\":{\"code\":2000,\"reason\":\"\"}}", false));
+//
+//    }
 
     @Test
     public void testDelete() throws Exception{
