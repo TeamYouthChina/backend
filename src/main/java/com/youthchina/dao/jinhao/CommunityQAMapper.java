1f3987a23d5f4dd08d6178f6c8d59f33cd7f8cd6
JinhaoHu <hujinhao0401@gmail.com>
2019-02-12 11:43:35 -0500

    backend-146 Service delete bug

--- src/main/java/com/youthchina/dao/jinhao/CommunityQAMapper.java
+++ src/main/java/com/youthchina/dao/jinhao/CommunityQAMapper.java
@@ -132,7 +132,9 @@ public interface CommunityQAMapper {
                                               @Param("comment_id") Integer comment_id,
                                               @Param("discuss_level") Integer discuss_level);
 
-    Integer deleteDiscuss(CommentDiscuss commentDiscuss);
+    void deleteDiscuss(Integer discuss_Id);
+
+    void deleteAllDiscussEvaluateByDiscussId(Integer discuss_id);
 
     Integer countDiscussAgreement(Integer discuss_id);
 
