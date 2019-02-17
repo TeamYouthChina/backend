33cedfc7e6e8a6c359563391ec3c74b7aefdc767
JinhaoHu <hujinhao0401@gmail.com>
2019-02-17 15:39:21 -0500

    短评

--- src/main/java/com/youthchina/domain/jinhao/communityQA/BriefReview.java
+++ src/main/java/com/youthchina/domain/jinhao/communityQA/BriefReview.java
@@ -1,6 +1,9 @@
 package com.youthchina.domain.jinhao.communityQA;
 
+import com.youthchina.domain.zhongyang.User;
+
 import java.sql.Timestamp;
+import java.util.List;
 
 public class BriefReview {
     private Integer review_id;
@@ -8,6 +11,18 @@ public class BriefReview {
     private Timestamp review_time;
     private Integer is_delete;
     private Timestamp is_delete_time;
+    private int rela_type;
+    private int rela_id;
+    private List<Comment> comments;
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public Integer getReview_id() {
         return review_id;
@@ -47,5 +62,29 @@ public class BriefReview {
 
     public void setIs_delete_time(Timestamp is_delete_time) {
         this.is_delete_time = is_delete_time;
+    }
+
+    public int getRela_type() {
+        return rela_type;
+    }
+
+    public void setRela_type(int rela_type) {
+        this.rela_type = rela_type;
+    }
+
+    public int getRela_id() {
+        return rela_id;
+    }
+
+    public void setRela_id(int rela_id) {
+        this.rela_id = rela_id;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 }
--- src/main/java/com/youthchina/service/jinhao/communityQA/BriefReviewServiceImplement.java
+++ src/main/java/com/youthchina/service/jinhao/communityQA/BriefReviewServiceImplement.java
@@ -1,52 +1,105 @@
 package com.youthchina.service.jinhao.communityQA;
 
+import com.youthchina.dao.jinhao.BriefReviewMapper;
 import com.youthchina.domain.jinhao.communityQA.*;
 import com.youthchina.exception.zhongyang.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class BriefReviewServiceImplement implements BriefReviewService {
+    @Resource
+    BriefReviewMapper briefReviewMapper;
 
     @Override
+    @Transactional
     public BriefReview add(BriefReview entity) {
-        return null;
+        briefReviewMapper.add(entity);
+        briefReviewMapper.createReviewMap(entity.getReview_id(), entity.getUser().getId(), entity.getRela_type()
+        ,entity.getRela_id());
+        return entity;
     }
 
     @Override
     public BriefReview get(Integer id) throws NotFoundException {
-        return null;
+        BriefReview briefReview = briefReviewMapper.get(id);
+        if(briefReview == null){
+            throw new NotFoundException(404,404,"This brief review does not exist!");
+        }else {
+            return briefReview;
+        }
     }
 
     @Override
     public List<BriefReview> get(List<Integer> id) throws NotFoundException {
-        return null;
+        List<BriefReview> briefReviews = briefReviewMapper.getList(id);
+        if(briefReviews.size() == 0){
+            throw new NotFoundException(404,404,"None of these review exists!");
+        }else {
+            return briefReviews;
+        }
+    }
+
+    private void checkIfReviewExist(Integer id) throws NotFoundException{
+        BriefReview briefReview = briefReviewMapper.simplyGetReview(id);
+        if(briefReview == null) throw new NotFoundException(404,404,"This brief review does not exist!");
     }
 
     @Override
+    @Transactional
     public BriefReview update(BriefReview briefReview) throws NotFoundException {
-        return null;
+        checkIfReviewExist(briefReview.getReview_id());
+        briefReviewMapper.update(briefReview);
+        return briefReview;
     }
 
     @Override
+    @Transactional
     public void delete(Integer id) throws NotFoundException {
-
+        checkIfReviewExist(id);
+        briefReviewMapper.delete(id);
+        briefReviewMapper.deleteAllReviewEvaluationByReviewId(id);
+        briefReviewMapper.deleteAllCommentsByReviewId(id);
+        briefReviewMapper.deleteAllCommentEvaluationByReviewId(id);
+        briefReviewMapper.deleteAllDiscussesByReviewId(id);
+        briefReviewMapper.deleteAllDiscussEvaluateByReviewId(id);
     }
 
     @Override
-    public Evaluate doEvaluate(Evaluate evaluate) throws NotFoundException {
-        return null;
+    @Transactional
+    public Evaluate doEvaluate(Integer user_id, Integer review_id, Integer evaluate_type) throws NotFoundException {
+        checkIfReviewExist(review_id);
+        Evaluate evaluate = briefReviewMapper.checkEvaluateStatus(user_id, review_id);
+        if(evaluate == null){
+            Evaluate evaluate1 = new Evaluate();
+            evaluate1.setUser_id(user_id);
+            evaluate1.setEvaluate_type(evaluate_type);
+            briefReviewMapper.addReviewEvaluation(evaluate1);
+            briefReviewMapper.createEvaluationReviewMap(evaluate1.getEvaluate_id(), review_id);
+            return evaluate1;
+        }else{
+            evaluate.setEvaluate_type(evaluate_type);
+            briefReviewMapper.updateEvaluation(evaluate);
+            return evaluate;
+        }
     }
 
     @Override
-    public Evaluate getEvaluate(Integer evaluate_id) throws NotFoundException {
-        return null;
+    @Transactional
+    public Integer getReviewAgreement(Integer review_id) throws NotFoundException{
+        checkIfReviewExist(review_id);
+        return briefReviewMapper.countReviewAgreement(review_id);
     }
 
     @Override
-    public Comment addComment(Comment comment) {
-        return null;
+    @Transactional
+    public Comment addComment(Comment comment, Integer review_id) {
+        briefReviewMapper.addComment(comment);
+        briefReviewMapper.createCommentReviewMap(comment.getComment_id(), null, review_id);
+        return comment;
     }
 
     @Override
