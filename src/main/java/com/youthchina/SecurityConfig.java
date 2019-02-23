3628fdcad0f489572ff4a89d4c288c97cd6da9f7
zhongyang.wu <zhongyang.wu@outlook.com>
2019-02-22 20:36:29 -0500

    CROS config

--- src/main/java/com/youthchina/SecurityConfig.java
+++ src/main/java/com/youthchina/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,8 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.net.URL;
-import java.util.Collections;
+import java.util.Arrays;
 
 /**
  * Created by zhongyangwu on 11/10/18.
@@ -73,8 +71,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));//todo: whileList
-        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTION", "HEAD"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
--- src/main/java/com/youthchina/YouthchinaApplication.java
+++ src/main/java/com/youthchina/YouthchinaApplication.java
@@ -2,11 +2,16 @@ package com.youthchina;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -15,10 +20,14 @@ import java.util.TimeZone;
 @Configuration
 @PropertySource("classpath:/config.properties")
 @MapperScan("com.youthchina.dao")
+@EnableWebMvc
 public class YouthchinaApplication {
 
     @Autowired
     private Environment env;
+
+    @Value("${security.token.header}")
+    private String jwtHeader;
 
     public static void main(String[] args) {
         SpringApplication.run(YouthchinaApplication.class, args);
@@ -27,6 +36,17 @@ public class YouthchinaApplication {
     @PostConstruct
     void started() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders(jwtHeader); //允许所有Origins
+            }
+        };
+
     }
 
 }
