8b25dfa18ce11e1a0357aa3d15c4529402d4c887
zhongyang.wu <zhongyang.wu@outlook.com>
2019-02-22 22:10:47 -0500

    fix bug

--- src/main/java/com/youthchina/YouthchinaApplication.java
+++ src/main/java/com/youthchina/YouthchinaApplication.java
@@ -2,16 +2,11 @@ package com.youthchina;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -20,14 +15,10 @@ import java.util.TimeZone;
 @Configuration
 @PropertySource("classpath:/config.properties")
 @MapperScan("com.youthchina.dao")
-@EnableWebMvc
 public class YouthchinaApplication {
 
     @Autowired
     private Environment env;
-
-    @Value("${security.token.header}")
-    private String jwtHeader;
 
     public static void main(String[] args) {
         SpringApplication.run(YouthchinaApplication.class, args);
@@ -38,15 +29,5 @@ public class YouthchinaApplication {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders(jwtHeader); //允许所有Origins
-            }
-        };
-
-    }
 
 }
