e5a75e5b1c595375e083a765eb58220794321282
IrvingZ <irvingzqy@gmail.com>
2019-02-27 11:44:14 -0500

    backend-68 API design MeController

---
+++ src/main/java/com/youthchina/controller/qingyang/MeController.java
@@ -0,0 +1,16 @@
+package com.youthchina.controller.qingyang;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: For /me
+ * @author: Qingyang Zhao
+ * @create: 2019-02-27
+ **/
+@RestController
+@RequestMapping("${web.url.prefix}/me")
+public class MeController {
+
+}
+
