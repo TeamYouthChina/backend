db0c9162fb27a18e772841ca810ce0bf5e11d0d9
IrvingZ <irvingzqy@gmail.com>
2019-02-27 12:06:18 -0500

    backend-68 API design MeController

--- src/main/java/com/youthchina/controller/qingyang/MeController.java
+++ src/main/java/com/youthchina/controller/qingyang/MeController.java
@@ -1,5 +1,15 @@
 package com.youthchina.controller.qingyang;
 
+import com.youthchina.domain.zhongyang.User;
+import com.youthchina.dto.Response;
+import com.youthchina.dto.StatusDTO;
+import com.youthchina.dto.UserDTO;
+import com.youthchina.exception.zhongyang.ForbiddenException;
+import com.youthchina.exception.zhongyang.NotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("${web.url.prefix}/me")
 public class MeController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> getMe(@AuthenticationPrincipal User user) throws ForbiddenException, NotFoundException {
+        if (user!=null) {
+            return ResponseEntity.ok(new Response(new UserDTO(user)));
+        } else {
+            throw new ForbiddenException();
+        }
+    }
 
 }
 
