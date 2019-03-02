3467dd6a10d28fdea31bb17aa8f922f081907ffb
Hongsheng Zhang <jonathan1zhang@gmail.com>
2019-03-02 14:41:21 -0500

    backend-233 Friend & Group Module

--- src/main/java/com/youthchina/service/Hongsheng/FriendServiceImplement.java
+++ src/main/java/com/youthchina/service/Hongsheng/FriendServiceImplement.java
@@ -1,7 +1,9 @@
 package com.youthchina.service.Hongsheng;
 
+/*
 import com.youthchina.dao.Hongsheng.FriendMapper;
 import com.youthchina.domain.tianjian.ComFriendRelation;
+import com.youthchina.domain.tianjian.ComFriendRelationMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +15,14 @@ public class FriendServiceImplement extends FriendService {
     FriendMapper friendMapper;
 
     @Override
-    public ComFriendRelation add(ComFriendRelation comFriendRelation) {
+    public ComFriendRelation add(ComFriendRelation comFriendRelation, Integer own_Id) {
         friendMapper.addFriendApplication(comFriendRelation);
+        ComFriendRelationMap comFriendRelationMap = new ComFriendRelationMap();
+        comFriendRelationMap.setRela_id(comFriendRelation.getRela_id());
+        comFriendRelationMap.setUser_id(own_Id);
+
 
     }
 
 }
+*/
