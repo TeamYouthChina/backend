e6e133289ffe5542dc6ed9f1b9df4743c20259a8
zhongyang.wu <zhongyang.wu@outlook.com>
2019-03-06 14:15:25 -0500

    backend-177 Security System

--- src/main/java/com/youthchina/controller/qingyang/MeController.java
+++ src/main/java/com/youthchina/controller/qingyang/MeController.java
@@ -2,14 +2,12 @@ package com.youthchina.controller.qingyang;
 
 import com.youthchina.domain.zhongyang.User;
 import com.youthchina.dto.Response;
-import com.youthchina.dto.StatusDTO;
 import com.youthchina.dto.UserDTO;
 import com.youthchina.exception.zhongyang.ForbiddenException;
 import com.youthchina.exception.zhongyang.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2019-02-27
  **/
 @RestController
-@RequestMapping("${web.url.prefix}/me")
+@RequestMapping("${web.url.prefix}/me/**")
 public class MeController {
 
-    @GetMapping("/")
+    @GetMapping("/**")
     public ResponseEntity<?> getMe(@AuthenticationPrincipal User user) throws ForbiddenException, NotFoundException {
         if (user!=null) {
             return ResponseEntity.ok(new Response(new UserDTO(user)));
