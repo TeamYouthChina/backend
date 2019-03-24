9487d473f7897186e74cecc118e584b31b29bcec
IrvingZ <irvingzqy@gmail.com>
2019-03-24 18:14:11 -0400

    backend-268 Company CRUD Company Employee

---
+++ src/main/java/com/youthchina/dao/qingyang/CompanyEmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.youthchina.dao.qingyang;
+
+import com.youthchina.domain.qingyang.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Mapper
+@Component
+public interface CompanyEmployeeMapper {
+
+}
+
---
+++ src/main/java/com/youthchina/service/qingyang/CompanyEmployeeCURDServiceImpl.java
@@ -0,0 +1,8 @@
+package com.youthchina.service.qingyang;
+
+/**
+ * @author: Qingyang Zhao
+ * @create: 2019-03-24
+ **/
+public class CompanyEmployeeCURDServiceImpl {
+}
