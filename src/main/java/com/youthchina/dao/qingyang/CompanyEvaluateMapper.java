7f43ab2851a0a86c09638695a620fe592a2a0c3d
IrvingZ <irvingzqy@gmail.com>
2019-03-24 17:39:44 -0400

    backend-268 Company CRUD Company Evaluate

---
+++ src/main/java/com/youthchina/dao/qingyang/CompanyEvaluateMapper.java
@@ -0,0 +1,17 @@
+package com.youthchina.dao.qingyang;
+
+import com.youthchina.domain.qingyang.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Mapper
+@Component
+public interface CompanyEvaluateMapper {
+
+    
+}
+
