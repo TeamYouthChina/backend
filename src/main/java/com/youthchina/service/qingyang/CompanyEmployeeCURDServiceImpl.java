20a284476446764e300db80d6aaf21f3dec7acb3
IrvingZ <irvingzqy@gmail.com>
2019-03-26 09:17:19 -0400

    backend-268 Company CRUD Company Employee

--- src/main/java/com/youthchina/service/qingyang/CompanyEmployeeCURDServiceImpl.java
+++ src/main/java/com/youthchina/service/qingyang/CompanyEmployeeCURDServiceImpl.java
@@ -1,8 +1,37 @@
 package com.youthchina.service.qingyang;
 
+import com.youthchina.domain.qingyang.CompanyEmployee;
+import com.youthchina.exception.zhongyang.NotFoundException;
+
+import java.util.List;
+
 /**
  * @author: Qingyang Zhao
  * @create: 2019-03-24
  **/
-public class CompanyEmployeeCURDServiceImpl {
+public class CompanyEmployeeCURDServiceImpl implements CompanyEmployeeCURDService{
+    @Override
+    public CompanyEmployee get(Integer id) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<CompanyEmployee> get(List<Integer> id) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) throws NotFoundException {
+
+    }
+
+    @Override
+    public CompanyEmployee update(CompanyEmployee companyEmployee) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public CompanyEmployee add(CompanyEmployee entity) {
+        return null;
+    }
 }
