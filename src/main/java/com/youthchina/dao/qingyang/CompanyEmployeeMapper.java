d48005f678dcb22324b9f008f7ef4bfe66d4ecaa
IrvingZ <irvingzqy@gmail.com>
2019-03-26 09:38:06 -0400

    backend-268 Company CRUD Company Employee

--- src/main/java/com/youthchina/dao/qingyang/CompanyEmployeeMapper.java
+++ src/main/java/com/youthchina/dao/qingyang/CompanyEmployeeMapper.java
@@ -2,7 +2,6 @@ package com.youthchina.dao.qingyang;
 
 import com.youthchina.domain.qingyang.*;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,6 +10,10 @@ import java.util.List;
 @Mapper
 @Component
 public interface CompanyEmployeeMapper {
-
+    CompanyEmployee getCompanyEmployee(Integer id);
+    List<CompanyEmployee> getCompanyEmployeeByIdList(List<Integer> idList);
+    Integer deleteCompanyEmployee(Integer id);
+    Integer updateCompanyEmployee(CompanyEmployee employee);
+    Integer insertCompanyEmployee(CompanyEmployee entity);
 }
 
--- src/main/java/com/youthchina/service/qingyang/CompanyEmployeeCURDServiceImpl.java
+++ src/main/java/com/youthchina/service/qingyang/CompanyEmployeeCURDServiceImpl.java
@@ -1,8 +1,10 @@
 package com.youthchina.service.qingyang;
 
+import com.youthchina.dao.qingyang.CompanyEmployeeMapper;
 import com.youthchina.domain.qingyang.CompanyEmployee;
 import com.youthchina.exception.zhongyang.NotFoundException;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -10,28 +12,33 @@ import java.util.List;
  * @create: 2019-03-24
  **/
 public class CompanyEmployeeCURDServiceImpl implements CompanyEmployeeCURDService{
+    @Resource
+    CompanyEmployeeMapper companyEmployeeMapper;
+
     @Override
     public CompanyEmployee get(Integer id) throws NotFoundException {
-        return null;
+        return companyEmployeeMapper.getCompanyEmployee(id);
     }
 
     @Override
-    public List<CompanyEmployee> get(List<Integer> id) throws NotFoundException {
-        return null;
+    public List<CompanyEmployee> get(List<Integer> idList) throws NotFoundException {
+        return companyEmployeeMapper.getCompanyEmployeeByIdList(idList);
     }
 
     @Override
     public void delete(Integer id) throws NotFoundException {
-
+        Integer result = companyEmployeeMapper.deleteCompanyEmployee(id);
     }
 
     @Override
     public CompanyEmployee update(CompanyEmployee companyEmployee) throws NotFoundException {
-        return null;
+        Integer result = companyEmployeeMapper.updateCompanyEmployee(companyEmployee);
+        return companyEmployeeMapper.getCompanyEmployee(companyEmployee.getCompanyId());
     }
 
     @Override
     public CompanyEmployee add(CompanyEmployee entity) {
-        return null;
+        Integer result = companyEmployeeMapper.insertCompanyEmployee(entity);
+        return companyEmployeeMapper.getCompanyEmployee(entity.getCompanyId());
     }
 }
