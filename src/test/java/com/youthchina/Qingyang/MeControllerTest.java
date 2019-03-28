827d75c2d91a11163b6830e81705bb8f8e5bc7d0
IrvingZ <irvingzqy@gmail.com>
2019-03-28 09:53:22 -0400

    MeController & New Test

--- src/test/java/com/youthchina/Qingyang/MeControllerTest.java
+++ src/test/java/com/youthchina/Qingyang/MeControllerTest.java
@@ -31,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class, TransactionalTestExecutionListener.class})
-@DatabaseSetup({"classpath:company.xml"})
+@DatabaseSetup({"classpath:New_SYS_test.xml"})
 @WebAppConfiguration
 public class MeControllerTest {
 
