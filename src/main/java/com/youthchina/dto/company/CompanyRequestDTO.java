cd98aa4756750699529ec2e08a5796ca2f2bd5ce
IrvingZ <irvingzqy@gmail.com>
2019-04-06 15:40:57 -0400

    backend-268 Company CRUD

--- src/main/java/com/youthchina/dto/company/CompanyRequestDTO.java
+++ src/main/java/com/youthchina/dto/company/CompanyRequestDTO.java
@@ -1,13 +1,8 @@
 package com.youthchina.dto.company;
 
-import com.youthchina.domain.Qinghong.Location;
-import com.youthchina.domain.qingyang.*;
+import com.youthchina.domain.qingyang.Company;
 import com.youthchina.dto.RequestDTO;
 import com.youthchina.dto.util.LocationDTO;
-import com.youthchina.dto.util.NationDTO;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author: Qingyang Zhao
@@ -19,10 +14,13 @@ public class CompanyRequestDTO implements RequestDTO<Company> {
     private String name;
     private LocationDTO location;
     private String website; // pattern: (http|https)://(.?)*
-    private NationDTO nation;
+    private String nation;
     private String avatarUrl;
     private String note;
     private Integer userId;
+
+    public CompanyRequestDTO() {
+    }
 
     public Integer getUserId() {
         return userId;
@@ -30,57 +28,6 @@ public class CompanyRequestDTO implements RequestDTO<Company> {
 
     public void setUserId(Integer userId) {
         this.userId = userId;
-    }
-
-    public CompanyRequestDTO() {
-    }
-
-    public CompanyRequestDTO(Company domain) {
-        this.id = domain.getCompanyId();
-        this.name = domain.getCompanyName();
-        this.location = new LocationDTO(domain.getLocation()); // Default: Chinese Location
-        this.website = domain.getCompanyWebsite();
-        this.nation = new NationDTO(domain.getCountry()); // Default: Chinese Location
-        List<Logo>  logoList = domain.getLogoList();
-        if(logoList != null && logoList.size() > 0){
-            this.avatarUrl = domain.getLogoList().get(0).getDocuLocalId();
-        }
-        this.note = domain.getCompanyIntroduc();
-        this.userId = domain.getUserId();
-    }
-
-    public Company setCompany(CompanyRequestDTO companyRequestDTO) {
-        Company company = new Company();
-        company.setCompanyId(companyRequestDTO.getId());
-        company.setCompanyName(companyRequestDTO.getName());
-        company.setLocation(new Location(companyRequestDTO.getLocation()));
-        company.setCompanyWebsite(companyRequestDTO.getWebsite());
-        company.setCountry(new Country(companyRequestDTO.getNation()));
-        company.setCompanyIntroduc(companyRequestDTO.getNote());
-        company.setUserId(companyRequestDTO.getUserId());
-
-        //TODO: Logo
-        List<Logo> logoList = new ArrayList<>();
-        Logo logo = new Logo();
-        logo.setDocuLocalId(companyRequestDTO.getAvatarUrl());
-        logoList.add(logo);
-        company.setLogoList(logoList);
-
-        //TODO : API need add more params as shown below
-
-        CompanyScale scale = new CompanyScale();
-        scale.setScaleNum(1);
-        company.setCompanyScale(scale);
-
-        CompanyNature nature = new CompanyNature();
-        nature.setNatureNum(1);
-        company.setCompanyNature(nature);
-
-        company.setCompanyMail("TODO@TODO.TODO");
-
-        company.setCompanyVerify(0);
-
-        return company;
     }
 
     public Integer getId() {
@@ -115,11 +62,11 @@ public class CompanyRequestDTO implements RequestDTO<Company> {
         this.website = website;
     }
 
-    public NationDTO getNation() {
+    public String getNation() {
         return nation;
     }
 
-    public void setNation(NationDTO nation) {
+    public void setNation(String nation) {
         this.nation = nation;
     }
 
