f3102b87d2a36c12ed3a0eeb462b8030fdcb3f9d
zhongyang.wu <zhongyang.wu@outlook.com>
2019-04-09 15:35:57 -0400

    refactor test

--- src/test/java/com/youthchina/Qingyang/CompanyEmployeeTest.java
+++ src/test/java/com/youthchina/Qingyang/CompanyEmployeeTest.java
@@ -24,7 +24,7 @@ import java.sql.Date;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class, TransactionalTestExecutionListener.class})
-@DatabaseSetup({"classpath:New_Company_test.xml", "classpath:companyEmployee.xml"})
+@DatabaseSetup({"classpath:test.xml"})
 public class CompanyEmployeeTest {
 
     @Autowired
--- src/test/java/com/youthchina/jinhao/EvaluateTest.java
+++ src/test/java/com/youthchina/jinhao/EvaluateTest.java
@@ -19,7 +19,7 @@ import javax.annotation.Resource;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class, TransactionalTestExecutionListener.class})
-@DatabaseSetup({"classpath:New_Community_test.xml","classpath:New_SYS_test.xml"})
+@DatabaseSetup({"classpath:test.xml"})
 public class EvaluateTest {
     @Resource
     EvaluateMapper evaluateMapper;
