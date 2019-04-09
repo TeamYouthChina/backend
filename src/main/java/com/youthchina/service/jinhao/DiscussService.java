82e65ea32c588f349b0ee5b1b4de4bd2c810867a
xiaoyi <937640235@qq.com>
2019-04-09 10:42:25 -0400

    new version

--- src/main/java/com/youthchina/service/jinhao/DiscussService.java
+++ src/main/java/com/youthchina/service/jinhao/DiscussService.java
@@ -3,7 +3,6 @@ package com.youthchina.service.jinhao;
 import com.youthchina.domain.jinhao.Discuss;
 import com.youthchina.exception.zhongyang.NotFoundException;
 import com.youthchina.service.DomainCRUDService;
-
 import java.util.List;
 
 public interface DiscussService extends DomainCRUDService<Discuss, Integer> {
@@ -13,11 +12,10 @@ public interface DiscussService extends DomainCRUDService<Discuss, Integer> {
      * @return a list of comment
      * @throws NotFoundException
      */
+
     List<Discuss> getDiscusses(Integer id) throws NotFoundException;
     List<Discuss> getDiscusses(Integer id, Integer start, Integer end);
     void isDiscussExist(Integer id) throws NotFoundException;
-
     void deleteAllDiscussOfComment(Integer id);
-
     Integer count(Integer id);
 }
