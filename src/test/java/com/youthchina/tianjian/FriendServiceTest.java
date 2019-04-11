9dc8ebd3007825d5808b2d735e63594dee51a512
Tianjian Chen <ctijia@sina.com>
2019-04-11 10:56:48 -0400

    Merge branch 'master' of https://github.com/TeamYouthChina/backend into Community
    # Conflicts:
    #	src/test/resources/essay.xml

--- src/test/java/com/youthchina/tianjian/FriendServiceTest.java
+++ src/test/java/com/youthchina/tianjian/FriendServiceTest.java
@@ -39,7 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class, TransactionalTestExecutionListener.class})
-@DatabaseSetup({"classpath:New_Community_test.xml"})
+@DatabaseSetup({"classpath:test.xml"})
 @WebAppConfiguration
 public class FriendServiceTest {
 
