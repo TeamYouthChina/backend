db5e8ca663f272d9d26ed23f79a329df62acd310
JinhaoHu <hujinhao0401@gmail.com>
2019-04-16 15:59:41 -0400

    11

--- src/test/java/com/youthchina/service/AnswerServiceTest.java
+++ src/test/java/com/youthchina/service/AnswerServiceTest.java
@@ -1,7 +1,6 @@
 package com.youthchina.service;
 
 import com.youthchina.dao.jinhao.AnswerMapper;
-import com.youthchina.domain.jinhao.Answer;
 import com.youthchina.service.community.*;
 import com.youthchina.service.user.UserServiceImpl;
 import org.junit.Before;
@@ -13,11 +12,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by zhongyangwu on 4/16/19.
@@ -55,12 +49,6 @@ public class AnswerServiceTest {
 
     @Test
     public void delete() throws Exception {
-        Answer answer = new Answer();
-        answer.setId(1);
-        when(this.answerMapper.get(1)).thenReturn(answer);
 
-        this.answerService.delete(1);
-        verify(attentionService).cancel(any(Answer.class));
-        verify(answerMapper).delete(anyInt());
     }
 }
