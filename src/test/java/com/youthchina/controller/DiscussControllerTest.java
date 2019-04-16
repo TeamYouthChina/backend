86764264e473cc827ac457fa70948614a9f7b5f5
xiaoyi <937640235@qq.com>
2019-04-16 09:26:34 -0400

    modify and add test case

--- src/test/java/com/youthchina/controller/DiscussControllerTest.java
+++ src/test/java/com/youthchina/controller/DiscussControllerTest.java
@@ -25,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @WebAppConfiguration
 public class DiscussControllerTest extends BaseControllerTest {
+
     @Resource
     EvaluateService evaluateService;
     @Test
