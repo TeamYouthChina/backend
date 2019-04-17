2233850376dab4aa1c330d4d39188dc7331619a5
Hongsheng Zhang <jonathan1zhang@gmail.com>
2019-04-17 12:11:40 -0400

    Add Recommend Controller Test

---
+++ src/test/java/com/youthchina/controller/RecommendControllerTest.java
@@ -0,0 +1,56 @@
+package com.youthchina.controller;
+
+import com.youthchina.util.AuthGenerator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class RecommendControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Value("${web.url.prefix}")
+    private String urlPrefix;
+
+    private AuthGenerator authGenerator = new AuthGenerator();
+
+    MockMvc mvc;
+
+    @Before
+    public void setup() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(context).apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    }
+
+    @Test
+    public void TestRecommend() throws Exception {
+        this.mvc.perform(
+                get(this.urlPrefix + "/search")
+                        .param("type", "all")
+                        .param("title", "腾")
+                        .with(authGenerator.authentication())
+        ).andDo(print())
+               .andExpect(status().is2xxSuccessful());
+
+    }
+}
