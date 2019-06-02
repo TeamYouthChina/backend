a13332b0098f55d0ea72ceedcd8433a14ba626c0
zhongyang.wu <zhongyang.wu@outlook.com>
2019-06-01 21:10:12 -0400

    backend-500 [FEATURE]简单推荐系统

--- src/main/java/com/youthchina/controller/user/TagController.java
+++ src/main/java/com/youthchina/controller/user/TagController.java
@@ -1,19 +1,37 @@
 package com.youthchina.controller.user;
 
+import com.youthchina.domain.jinhao.BriefReview;
 import com.youthchina.domain.jinhao.Label;
-
+import com.youthchina.domain.jinhao.Question;
+import com.youthchina.domain.qingyang.Job;
+import com.youthchina.domain.tianjian.ComEssay;
+import com.youthchina.domain.zhongyang.Role;
+import com.youthchina.domain.zhongyang.User;
 import com.youthchina.dto.Response;
 import com.youthchina.dto.StatusDTO;
-
 import com.youthchina.dto.tag.TagRequestDTO;
 import com.youthchina.dto.tag.TagResponseDTO;
+import com.youthchina.exception.zhongyang.exception.ClientException;
+import com.youthchina.exception.zhongyang.exception.ForbiddenException;
+import com.youthchina.exception.zhongyang.exception.InternalStatusCode;
 import com.youthchina.exception.zhongyang.exception.NotFoundException;
+import com.youthchina.service.application.CompanyCURDService;
+import com.youthchina.service.application.JobService;
+import com.youthchina.service.community.BriefReviewService;
+import com.youthchina.service.community.EssayService;
+import com.youthchina.service.community.QuestionService;
 import com.youthchina.service.recommendation.RecommendServiceImpl;
+import com.youthchina.service.user.UserService;
+import com.youthchina.util.dictionary.TagTargetType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -27,28 +45,93 @@ public class TagController {
     @Autowired
     private RecommendServiceImpl recommendService;
 
+    @Resource
+    BriefReviewService briefReviewService;
+
+    @Resource
+    EssayService essayService;
+
+    @Resource
+    QuestionService questionService;
+
+    @Resource
+    JobService jobService;
+
+    @Resource
+    CompanyCURDService companyCURDService;
+
+    @Resource
+    UserService userService;
+
 
     @GetMapping("/{type}/{id}")
-    public ResponseEntity getTags(@PathVariable Integer id, @PathVariable Integer type){
-        List<Label> labelList = recommendService.getLabels(type,id);
+    public ResponseEntity getTags(@PathVariable Integer id, @PathVariable Integer type) {
+        List<Label> labelList = recommendService.getLabels(type, id);
         List<TagResponseDTO> tagResponseDTOS = new ArrayList<>();
         Iterator iterator = labelList.iterator();
-        while (iterator.hasNext()){
-            Label label = (Label)iterator.next();
+        while (iterator.hasNext()) {
+            Label label = (Label) iterator.next();
             TagResponseDTO tagResponseDTO = new TagResponseDTO(label);
             tagResponseDTOS.add(tagResponseDTO);
         }
-        return ResponseEntity.ok(new Response(tagResponseDTOS,new StatusDTO(201, "success")));
+        return ResponseEntity.ok(new Response(tagResponseDTOS, new StatusDTO(201, "success")));
     }
+
     @PostMapping
-    public ResponseEntity addTags(@RequestBody TagRequestDTO tagRequestDTO)throws NotFoundException{
-        recommendService.addTag(tagRequestDTO.getLabel_code(),tagRequestDTO.getTarget_type(),tagRequestDTO.getTarget_id());
-        return ResponseEntity.ok(new Response(new StatusDTO(201, "success")));
+    public ResponseEntity addTags(@RequestBody TagRequestDTO tagRequestDTO, @AuthenticationPrincipal User user) throws NotFoundException, ForbiddenException, ClientException {
+        //role control
+        if (this.allowAccess(tagRequestDTO, user)) {
+            throw new ForbiddenException(InternalStatusCode.ACCESS_DENY);
+        }
+        if (!recommendService.isTagExist(tagRequestDTO.getLabel_code(), tagRequestDTO.getTarget_type(), tagRequestDTO.getTarget_id())) {
+            recommendService.addTag(tagRequestDTO.getLabel_code(), tagRequestDTO.getTarget_type(), tagRequestDTO.getTarget_id());
+            return ResponseEntity.ok(new Response(new StatusDTO(2010, "success")));
+        } else {
+            recommendService.addTag(tagRequestDTO.getLabel_code(), tagRequestDTO.getTarget_type(), tagRequestDTO.getTarget_id());
+            return ResponseEntity.ok(new Response(new StatusDTO(2000, "the tag is already exist")));
+        }
+    }
+
+    private boolean allowAccess(@Nullable TagRequestDTO tagRequestDTO, @Nonnull User user) throws NotFoundException, ClientException {
+        if (tagRequestDTO == null || tagRequestDTO.getTarget_id() == null) {
+            throw new ClientException("no payload is specific, payload cannot interpreted or no valid target id is provided");
+        }
+        switch (tagRequestDTO.getTarget_type()) {
+            case TagTargetType.QUESTION: {
+                Question question = questionService.get(tagRequestDTO.getTarget_id());
+                return question != null && question.getUser().equals(user);
+            }
+            case TagTargetType.ARTICLE: {
+                ComEssay comEssay = essayService.get(tagRequestDTO.getTarget_id());
+                return comEssay != null && comEssay.getAuthor().equals(user);
+            }
+            case TagTargetType.EDITORIAL: {
+                BriefReview briefReview = briefReviewService.get(tagRequestDTO.getTarget_id());
+                return briefReview != null && briefReview.getUser().equals(user);
+            }
+            case TagTargetType.COMPANY: {
+                for (GrantedAuthority authority : user.getAuthorities()) {
+                    if (authority.getAuthority().equals(Role.ROOT.getAuthority()) || authority.getAuthority().equals(Role.ADMIN.getAuthority())) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            case TagTargetType.JOB: {
+                Job job = jobService.get(tagRequestDTO.getTarget_id());
+                return job != null && job.getUserId().equals(user.getId());
+            }
+            case TagTargetType.USER: {
+                return tagRequestDTO.getTarget_id().equals(user.getId());
+            }
+            default:
+                throw new ClientException("invalid label type");
+        }
     }
 
     @DeleteMapping("/{labelCode}/{targetType}/{targetId}")
-    public ResponseEntity deleteTags(@PathVariable String labelCode,@PathVariable Integer targetType,@PathVariable Integer targetId)throws NotFoundException{
-        recommendService.deleteTag(labelCode,targetType,targetId);
+    public ResponseEntity deleteTags(@PathVariable String labelCode, @PathVariable Integer targetType, @PathVariable Integer targetId) throws NotFoundException {
+        recommendService.deleteTag(labelCode, targetType, targetId);
         return ResponseEntity.ok(new Response(new StatusDTO(200, "success")));
     }
 }
--- src/main/java/com/youthchina/service/recommendation/RecommendServiceImpl.java
+++ src/main/java/com/youthchina/service/recommendation/RecommendServiceImpl.java
@@ -15,6 +15,7 @@ import com.youthchina.service.community.BriefReviewService;
 import com.youthchina.service.community.EssayService;
 import com.youthchina.service.community.QuestionService;
 import com.youthchina.service.user.UserService;
+import com.youthchina.util.dictionary.TagTargetType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,51 +46,67 @@ public class RecommendServiceImpl implements RecommendService {
     UserService userService;
 
 
-
     @Override
     @Transactional
     public void addTag(String labelCode, int targetType, int targetId) throws NotFoundException {
-        switch (targetType){
-            case 1: questionService.get(targetId); break;
-            case 2: essayService.get(targetId); break;
-            case 3: briefReviewService.get(targetId); break;
-            case 100: userService.get(targetId); break;
-            case 200: companyCURDService.get(targetId); break;
-            case 300: jobService.get(targetId); break;
-            default: throw new NotFoundException(404,404,"The target type does not exist!");
+        //check whether targetId exist
+        if (targetType == TagTargetType.QUESTION) {
+            questionService.get(targetId);
         }
-        recommendMapper.addTag(labelCode,targetType,targetId);
+        if (targetType == TagTargetType.ARTICLE) {
+            essayService.get(targetId);
+        }
+        if (targetType == TagTargetType.EDITORIAL) {
+            briefReviewService.get(targetId);
+        }
+        if (targetType == TagTargetType.USER) {
+            userService.get(targetId);
+        }
+        if (targetType == TagTargetType.COMPANY) {
+            companyCURDService.get(targetId);
+        }
+        if (targetType == TagTargetType.JOB) {
+            jobService.get(targetId);
+        } else {
+            //default
+            throw new NotFoundException(404, 404, "The target type does not exist!");
+        }
+        recommendMapper.addTag(labelCode, targetType, targetId);
     }
 
     @Override
     @Transactional
     public void deleteTag(String labelCode, int targetType, int targetId) throws NotFoundException {
-        String id = recommendMapper.isTagExist(labelCode,targetType,targetId);
-        if(id == null) throw new NotFoundException(404,404,"This label does not exist and cannot be deleted");
-        recommendMapper.deleteTag(labelCode,targetType,targetId);
+        String id = recommendMapper.isTagExist(labelCode, targetType, targetId);
+        if (id == null) throw new NotFoundException(404, 404, "This label does not exist and cannot be deleted");
+        recommendMapper.deleteTag(labelCode, targetType, targetId);
+    }
+
+    public boolean isTagExist(String labelCode, int targetType, int targetId) {
+        return recommendMapper.isTagExist(labelCode, targetType, targetId) != null;
     }
 
     @Override
     public List<Label> getLabels(int targetType, int targetId) {
-        List<String> labelCodes = recommendMapper.getLabelCode(targetType,targetId);
+        List<String> labelCodes = recommendMapper.getLabelCode(targetType, targetId);
         List<Label> labels = new ArrayList<>();
-        if(labelCodes.size() == 0) return labels;
+        if (labelCodes.size() == 0) return labels;
         labels = recommendMapper.getLabel(labelCodes);
         return labels;
     }
 
     @Override
     @Transactional
-    public List<User> getRecommendUser(int userId) throws NotFoundException{
+    public List<User> getRecommendUser(int userId) throws NotFoundException {
         List<String> userLabels = recommendMapper.getUserLabel(userId);
-        if(userLabels.size() == 0){
-            throw new NotFoundException(404,404,"User do not have any labels");
+        if (userLabels.size() == 0) {
+            throw new NotFoundException(404, 404, "User do not have any labels");
         }
         List<Integer> userIds = recommendMapper.getRecommendUser(userLabels);
         Set<Integer> nonRepeatIds = new HashSet<>(userIds);
         List<User> users = new LinkedList<>();
-        for(Integer id : nonRepeatIds){
-            if(id == userId) continue;
+        for (Integer id : nonRepeatIds) {
+            if (id == userId) continue;
             try {
                 users.add(userService.get(id));
             } catch (NotFoundException e) {
@@ -101,10 +118,10 @@ public class RecommendServiceImpl implements RecommendService {
 
     @Override
     @Transactional
-    public List<Company> getRecommendCompany(int userId) throws NotFoundException{
+    public List<Company> getRecommendCompany(int userId) throws NotFoundException {
         List<String> userLabels = recommendMapper.getUserLabel(userId);
-        if(userLabels.size() == 0){
-            throw new NotFoundException(404,404,"User do not have any labels");
+        if (userLabels.size() == 0) {
+            throw new NotFoundException(404, 404, "User do not have any labels");
         }
         List<Integer> ids = recommendMapper.getRecommendCompany(userLabels);
         Set<Integer> reduceRepeat = new HashSet<>(ids);
@@ -114,10 +131,10 @@ public class RecommendServiceImpl implements RecommendService {
 
     @Override
     @Transactional
-    public List<ComEssay> getRecommendEssay(int userId) throws NotFoundException{
+    public List<ComEssay> getRecommendEssay(int userId) throws NotFoundException {
         List<String> userLabels = recommendMapper.getUserLabel(userId);
-        if(userLabels.size() == 0){
-            throw new NotFoundException(404,404,"User do not have any labels");
+        if (userLabels.size() == 0) {
+            throw new NotFoundException(404, 404, "User do not have any labels");
         }
         List<Integer> ids = recommendMapper.getRecommendEassy(userLabels);
         Set<Integer> reduceRepeat = new HashSet<>(ids);
@@ -126,10 +143,10 @@ public class RecommendServiceImpl implements RecommendService {
     }
 
     @Override
-    public List<Question> getRecommendQuestion(int userId) throws NotFoundException{
+    public List<Question> getRecommendQuestion(int userId) throws NotFoundException {
         List<String> userLabels = recommendMapper.getUserLabel(userId);
-        if(userLabels.size() == 0){
-            throw new NotFoundException(404,404,"User do not have any labels");
+        if (userLabels.size() == 0) {
+            throw new NotFoundException(404, 404, "User do not have any labels");
         }
         List<Integer> ids = recommendMapper.getRecommendQuestion(userLabels);
         Set<Integer> reduceRepeat = new HashSet<>(ids);
@@ -138,10 +155,10 @@ public class RecommendServiceImpl implements RecommendService {
     }
 
     @Override
-    public List<Job> getRecommendJob(int userId) throws NotFoundException{
+    public List<Job> getRecommendJob(int userId) throws NotFoundException {
         List<String> userLabels = recommendMapper.getUserLabel(userId);
-        if(userLabels.size() == 0){
-            throw new NotFoundException(404,404,"User do not have any labels");
+        if (userLabels.size() == 0) {
+            throw new NotFoundException(404, 404, "User do not have any labels");
         }
         List<Integer> ids = recommendMapper.getRecommendJob(userLabels);
         Set<Integer> reduceRepeat = new HashSet<>(ids);
@@ -150,10 +167,10 @@ public class RecommendServiceImpl implements RecommendService {
     }
 
     @Override
-    public List<BriefReview> getRecommendBriefReview(int userId) throws NotFoundException{
+    public List<BriefReview> getRecommendBriefReview(int userId) throws NotFoundException {
         List<String> userLabels = recommendMapper.getUserLabel(userId);
-        if(userLabels.size() == 0){
-            throw new NotFoundException(404,404,"User do not have any labels");
+        if (userLabels.size() == 0) {
+            throw new NotFoundException(404, 404, "User do not have any labels");
         }
         List<Integer> ids = recommendMapper.getRecommendBriefReview(userLabels);
         Set<Integer> reduceRepeat = new HashSet<>(ids);
