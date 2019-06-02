72e81844937929684c01b673ed3a5232fe4602e9
zhongyang.wu <zhongyang.wu@outlook.com>
2019-06-02 11:30:16 -0400

    backend-551 [BUG]

--- src/main/java/com/youthchina/domain/zhongyang/Role.java
+++ src/main/java/com/youthchina/domain/zhongyang/Role.java
@@ -1,6 +1,7 @@
 package com.youthchina.domain.zhongyang;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 /**
  * Created by zhongyangwu on 2/20/19.
@@ -21,5 +22,9 @@ public enum Role implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return this.name();
+    }
+
+    public SimpleGrantedAuthority getSimpleGrantedAuthority(){
+        return new SimpleGrantedAuthority(this.name());
     }
 }
